7a5e76f9bc556e1ee851c646a03fdce70c7ac961
aarikmudgal <aarikmudgal@gmail.com>
2018-03-31 16:30:10 +0530

    Update urlConstants.js

--- app/containers/App/appconstants/urlConstants.js
+++ app/containers/App/appconstants/urlConstants.js
@@ -1,9 +1,9 @@
 var urls = {
-  login: 'http://10.31.87.23:81/api/customers/login',
-  articleList: 'http://10.31.87.23:82/api/articles',
-  orders: 'http://10.31.87.23:83/api/orders',
-  orderDetails: 'http://10.31.87.23:83/api/orders/[ORDER_ID]',
-  articleAdd: 'http://10.31.87.23:83/api/orders/[ORDER_ID]/articles',
-  articleDelete:'http://10.31.87.23:83/api/orders/[ORDER_ID]/articles/[ARTICLE_ID]'
+  login: 'http://35.200.145.203/api/customers/login',
+  articleList: 'http://35.200.247.55/api/articles',
+  orders: 'http://35.200.172.68/api/orders',
+  orderDetails: 'http://35.200.172.68/api/orders/[ORDER_ID]',
+  articleAdd: 'http://35.200.172.68/api/orders/[ORDER_ID]/articles',
+  articleDelete:'http://35.200.172.68/api/orders/[ORDER_ID]/articles/[ARTICLE_ID]'
 }
 module.exports = urls;
