2b15d32d173bc84d97b19282a7d94e572c207ee8
aarikmudgal <aarikmudgal@gmail.com>
2018-04-02 15:35:24 +0530

    Update index.js

--- app/containers/LoginPage/index.js
+++ app/containers/LoginPage/index.js
@@ -94,10 +94,7 @@ export class LoginPage extends React.PureComponent { // eslint-disable-line reac
           console.log(response);
           window.sessionStorage.setItem('CustomerId', response.data.Customer.CustomerId);
           window.sessionStorage.setItem('CustomerName', response.data.Customer.FirstName+" "+response.data.Customer.LastName);
-<<<<<<< HEAD
-         window.sessionStorage.getItem('cartItemsCount')
-=======
->>>>>>> 7a5e76f9bc556e1ee851c646a03fdce70c7ac961
+          window.sessionStorage.getItem('cartItemsCount')
           me.props.history.push('/articleDetails');
         })
         .catch(function (error) {
