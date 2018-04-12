55ab7596e5e47aab6bf3a531fb3c6adb29ca9d55
aarikmudgal <aarikmudgal@gmail.com>
2018-04-12 12:14:11 +0530

    Update urlConstants.js

--- app/containers/App/appconstants/urlConstants.js
+++ app/containers/App/appconstants/urlConstants.js
@@ -5,11 +5,11 @@ var urls = {
   articleImage: '/api/articles/[IMAGE_URL]',
   orders: '/api/orders',
   orderDetails: '/api/orders/[ORDER_ID]',
+  orderSubmit: '/api/orders/[ORDER_ID]/status/3', // 3- submitted
   articleAdd: '/api/orders/[ORDER_ID]/articles',
   articleDelete: '/api/orders/[ORDER_ID]/articles/[ARTICLE_ID]',
   articleListHealth: '/api/articles/health',
   ordersHealth: '/api/orders/health',
   customersHealth: '/api/customers/health',
-
 }
 module.exports = urls;
